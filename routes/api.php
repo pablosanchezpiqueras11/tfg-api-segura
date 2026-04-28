999e1229127232cab936d74008fe996df4843773
pablosanchezpiqueras11 <psanpiq@teleco.upv.es>
2026-04-28 12:56:43 +0200

    Primer endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Primer punto de entrada (Endpoint)
+Route::get('/saludo', function () {
+    return response()->json([
+        'mensaje' => 'Hola Mundo, mi API del TFG está viva',
+        'autor' => 'Pablo',
+        'estado' => 'Entorno configurado correctamente',
+        'fecha' => date('Y-m-d H:i:s')
+    ]);
+});
