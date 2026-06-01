de0a0591f19d6a960e8d1e0a00c08ef16ce5690f
pablosanchezpiqueras11 <psanpiq@teleco.upv.es>
2026-06-01 23:43:09 +0200

    error 429

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,4 +26,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->render(function (\Spatie\Permission\Exceptions\UnauthorizedException $e, $request) {
             return response()->json(['message' => 'No tienes permisos para realizar esta acción'], 403);
         });
+
+        $exceptions->render(function (\Illuminate\Http\Exceptions\ThrottleRequestsException $e, $request) {
+            return response()->json(['message' => 'Demasiadas peticiones. Inténtalo más tarde.'], 429);
+        });
     })->create();
